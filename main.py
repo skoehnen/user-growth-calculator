417fb5f7620ddd3a62eea015319d16e886a9284b
Stefan Köhnen <s.koehnen@fz-juelich.de>
2022-10-20 13:45:03 +0200

    Add main function

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ logging.basicConfig(level=logging.INFO)
 def read_csv_file(fileName):
     logging.info("Read csv function called")
 
+def main():
+    logging.info("Main function called")
+
 if __name__ == '__main__':
     print("User growth calculator")
     # Read input file-name from command parameter
