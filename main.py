97da9a4c5c3d9595cf4ddeedb568a10342efaa00
Stefan Köhnen <s.koehnen@fz-juelich.de>
2022-10-20 13:47:01 +0200

    Update log to show whole content of argv

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ logging.basicConfig(level=logging.INFO)
 def read_csv_file(fileName):
     logging.info("Read csv function called")
 
-def main():
+def splitArgv(argv):
+
+
+def main(argv):
     logging.info("Main function called")
 
 if __name__ == '__main__':
     print("User growth calculator")
     # Read input file-name from command parameter
     inputFile = sys.argv[1]
-    logging.info(f"Input file: {inputFile}")
+    logging.info(f"argv: {sys.argv}")
     print(inputFile)
