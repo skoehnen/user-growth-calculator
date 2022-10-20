b33ea04cdd462500b68dd25e0ee6cc965cdf2006
Stefan Köhnen <s.koehnen@fz-juelich.de>
2022-10-20 13:51:23 +0200

    Add logging and exception for incorrect number of arguments

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ def read_csv_file(fileName):
     logging.info("Read csv function called")
 
 def splitArgv(argv):
-
+    logging.info("splitArgv function called")
+    if len(argv) != 2:
+        logging.critical("Not the correct number of arguments")
+        raise Exception("Not the correct number of arguments")
 
 def main(argv):
     logging.info("Main function called")
+    splitArgv(argv)
 
 if __name__ == '__main__':
     print("User growth calculator")
     # Read input file-name from command parameter
     inputFile = sys.argv[1]
     logging.info(f"argv: {sys.argv}")
+    main(sys.argv)
