f999eed6da1bb2202beddeb11ade9fa30a95cebe
Stefan Köhnen <s.koehnen@fz-juelich.de>
2022-10-20 13:40:50 +0200

    Add variable to store input file-name

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ def read_csv_file(fileName):
 if __name__ == '__main__':
     print("User growth calculator")
     # Read input file-name from command parameter
-    print(sys.argv[1])
+    inputFile = sys.argv[1]
+    print(inputFile)
