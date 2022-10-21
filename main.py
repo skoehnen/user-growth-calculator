2c3f4516a4ba3fd068c2d139218272e2dcd43c9d
Stefan Köhnen <s.koehnen@fz-juelich.de>
2022-10-21 14:54:55 +0200

    Calculate monthly number users

--- main.py
+++ main.py
@@ -43,10 +43,25 @@ def main(argv):
 
     start_date_range = getDateRange(getStartDate(data), getEndDate(data))
 
+    monthList = []
+    userCountPerMonth = []
+
     for i in zip(start_date_range[:-1], start_date_range[1:]):
         filtered_df = data.loc[(data['Date'] >= i[0]) & (data['Date'] < i[1])]
-        print(len(filtered_df))
+        print(f"{i[0].strftime('%Y-%m')}: {len(filtered_df)}")
+        monthList.append(i[0].strftime('%Y-%m'))
+        userCountPerMonth.append(len(filtered_df))
 
+    print(monthList)
+    print(userCountPerMonth)
+    
+    for i in zip(monthList, userCountPerMonth):
+        print(i)
+
+    df = pandas.DataFrame(zip(monthList, userCountPerMonth), columns=['Month', 'Number of user'])
+    print(df)
+
+    df.to_csv("test.csv")
 
 if __name__ == '__main__':
     print("User growth calculator")
